a104091c1f1f802cbcade6d58325c0a584b787f0
Mohammed Nazar <x3raqe@gmail.com>
2024-02-29 17:03:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+
+function superbowlWin(arr){
+   const recObj = arr.find(i => i.result === "W"? i.year : undefined)
+
+    if(recObj){
+        return recObj.year;
+    } else{
+       return undefined
+    }
+}
+
